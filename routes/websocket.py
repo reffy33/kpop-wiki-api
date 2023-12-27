943f5e5e3918af5406477aa269c85a7fb9de24b4
reffy33 <reffymin@gmail.com>
2023-12-27 19:10:39 +0500

    websocket fixed bug

--- routes/websocket.py
+++ routes/websocket.py
@@ -35,7 +35,7 @@ async def websocket_endpoint(wb: WebSocket, client_id: int):
     try:
         while True:
             data = await wb.receive_text()
-            await manager.broadcast(f"#{client_id}: {data}")
+            await send_broadcast(f"#{client_id}: {data}")
     except WebSocketDisconnect:
         manager.disconnect(wb)
         await send_broadcast(f"The client {client_id} has left the chat")
